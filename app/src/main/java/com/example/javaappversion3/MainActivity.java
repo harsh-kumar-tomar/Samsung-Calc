afbf628e9337f62823978d98136bd025f8c0de1b
Harsh Kumar Tomar <harshpiyush2027@gmail.com>
2024-04-04 03:18:25 +0530

    Using MVVM

--- app/src/main/java/com/example/javaappversion3/MainActivity.java
+++ app/src/main/java/com/example/javaappversion3/MainActivity.java
@@ -10,7 +10,10 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.javaappversion3.databinding.ActivityMainBinding;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
@@ -26,20 +29,23 @@ public class MainActivity extends AppCompatActivity {
     String datatoCalculate = "" ;
     Logic logic;
     HashMap<Character,Integer> signHashMap = new HashMap<>();
+    ActivityMainBinding binding;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        currentOrientation = getResources().getConfiguration().orientation;
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+//        currentOrientation = getResources().getConfiguration().orientation;
 
         logic = new ViewModelProvider(this)
                 .get(Logic.class);
 
-        intialize();
-        txt_input.setText(logic.getCurrentInput());
-        txt_answer.setText(logic.getResult());
+//        intialize();
+        binding.textViewInput.setText(logic.getCurrentInput());
+        binding.textViewAnswer.setText(logic.getResult());
+
         signHashMap.put('+',1);
         signHashMap.put('-',1);
         signHashMap.put('/',1);
@@ -49,45 +55,44 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void intialize() {
-
-        but0 =findViewById(R.id.but0);
-        but1 =findViewById(R.id.but1);
-        but2 =findViewById(R.id.but2);
-        but3 =findViewById(R.id.but3);
-        but4 =findViewById(R.id.but4);
-        but5 =findViewById(R.id.but5);
-        but6 =findViewById(R.id.but6);
-        but7 =findViewById(R.id.but7);
-        but8 =findViewById(R.id.but8);
-        but9 =findViewById(R.id.but9);
-        but_equal =findViewById(R.id.but_equal);
-        but_add =findViewById(R.id.but_add);
-        but_div =findViewById(R.id.but_divide);
-        but_mult =findViewById(R.id.but_multiply);
-        but_sub =findViewById(R.id.but_subtract);
-        but_percent =findViewById(R.id.but_percent);
-        but_C =findViewById(R.id.but_c);
-        but_decimal =findViewById(R.id.but_decimal);
-        but_delete =findViewById(R.id.but_delete);
-
-        txt_answer =findViewById(R.id.textView_answer);
-        txt_input =findViewById(R.id.textView_input);
-
-
-    }
-
-    public void rotate(View view)
-    {
-        if (currentOrientation == Configuration.ORIENTATION_PORTRAIT) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-            currentOrientation = Configuration.ORIENTATION_LANDSCAPE;
-        } else if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-            currentOrientation = Configuration.ORIENTATION_PORTRAIT;
-        }
-
-    }
+//    public void intialize() {
+//
+//        but0 =findViewById(R.id.but0);
+//        but1 =findViewById(R.id.but1);
+//        but2 =findViewById(R.id.but2);
+//        but3 =findViewById(R.id.but3);
+//        but4 =findViewById(R.id.but4);
+//        but5 =findViewById(R.id.but5);
+//        but6 =findViewById(R.id.but6);
+//        but7 =findViewById(R.id.but7);
+//        but8 =findViewById(R.id.but8);
+//        but9 =findViewById(R.id.but9);
+//        but_equal =findViewById(R.id.but_equal);
+//        but_add =findViewById(R.id.but_add);
+//        but_div =findViewById(R.id.but_divide);
+//        but_mult =findViewById(R.id.but_multiply);
+//        but_sub =findViewById(R.id.but_subtract);
+//        but_percent =findViewById(R.id.but_percent);
+//        but_C =findViewById(R.id.but_c);
+//        but_decimal =findViewById(R.id.but_decimal);
+//        but_delete =findViewById(R.id.but_delete);
+//
+//        txt_answer =findViewById(R.id.textView_answer);
+//        txt_input =findViewById(R.id.textView_input);
+//
+//
+//    }
+//    public void rotate(View view)
+//    {
+//        if (currentOrientation == Configuration.ORIENTATION_PORTRAIT) {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//            currentOrientation = Configuration.ORIENTATION_LANDSCAPE;
+//        } else if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+//            currentOrientation = Configuration.ORIENTATION_PORTRAIT;
+//        }
+//
+//    }
 
 
     public void buttonClicked(View view)
@@ -96,8 +101,8 @@ public class MainActivity extends AppCompatActivity {
 
         if (currentButtonText.equals("="))
         {
-            txt_input.setText(logic.getResult());
-            txt_answer.setText("");
+            binding.textViewInput.setText(logic.getResult());
+            binding.textViewAnswer.setText("");
             logic.setCurrentInput(logic.getResult());
             logic.currentInput = new StringBuilder();
             return ;
@@ -110,11 +115,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         logic.setCurrentInput(currentButtonText);
-        txt_input.setText(logic.getCurrentInput());      //setting number in the input panel
+        binding.textViewInput.setText(logic.getCurrentInput());      //setting number in the input panel
 
         if (!signHashMap.containsKey(currentButtonText.charAt(0)))
         {
-            txt_answer.setText(logic.getResult());      //setting number in the input panel
+            binding.textViewAnswer.setText(logic.getResult());      //setting number in the input panel
         }
 
     }
@@ -124,8 +129,8 @@ public class MainActivity extends AppCompatActivity {
         if (!logic.getCurrentInput().equals(""))
         {
             logic.deleteSingleCharacter();
-            txt_input.setText(logic.getCurrentInput());      //setting number in the input panel
-            txt_answer.setText(logic.getCurrentInput());      //setting number in the input panel
+            binding.textViewInput.setText(logic.getCurrentInput());      //setting number in the input panel
+            binding.textViewAnswer.setText(logic.getCurrentInput());      //setting number in the input panel
         }
 
     }
@@ -134,8 +139,8 @@ public class MainActivity extends AppCompatActivity {
     public void clearInput()
     {
         logic.currentInput= new StringBuilder();
-        txt_input.setText(logic.getCurrentInput());
-        txt_answer.setText(logic.getResult());
+        binding.textViewInput.setText(logic.getCurrentInput());
+        binding.textViewAnswer.setText(logic.getResult());
     }
 
 
